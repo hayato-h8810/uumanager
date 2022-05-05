a81b2a56970d6d969da6e33d49ce059fac36f0df
hayato <hayato@hayatonoMacBook-Pro.local>
2022-05-05 21:46:15 +0900

    url編集時、新しくフォルダーを追加可能に変更 #64

--- api/app/graphql/mutations/edit_url.rb
+++ api/app/graphql/mutations/edit_url.rb
@@ -5,13 +5,27 @@ module Mutations
     argument :url, InputTypes::UrlInput, required: true
     argument :url_id, String, required: true
     argument :folder_id, String, required: false
+    argument :new_folder_name, String, required: false
 
     type [ObjectTypes::Folder]
 
-    def resolve(url:, url_id:, folder_id: nil)
+    def resolve(url:, url_id:, folder_id: nil, new_folder_name: nil)
       return unless editUrl = Url.find_by(id: url_id)
 
-      if folder_id
+      user = context[:current_user]
+
+      if new_folder_name
+        previousFolderId = editUrl.folder_id
+        newFolder = user.folders.create(name: new_folder_name)
+        editUrl.update(title: url.title,
+                       memo: url.memo,
+                       url: url.url,
+                       importance: url.importance,
+                       notification: url.notification,
+                       folder_id: newFolder.id)
+
+        [Folder.find(previousFolderId), Folder.find(newFolder.id)]
+      elsif folder_id
         previousFolderId = editUrl.folder_id
         editUrl.update(title: url.title,
                        memo: url.memo,
