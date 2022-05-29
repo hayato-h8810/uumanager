c9abc5f3e12a66bddec72cbfe896a56e891ae8d6
hayato <hayato@hayatonoMacBook-Pro.local>
2022-05-29 12:18:21 +0900

    not foundページのuiを作成 #124

--- frontend/src/components/pageNotFound/pageNotFoundContainer.tsx
+++ frontend/src/components/pageNotFound/pageNotFoundContainer.tsx
@@ -1,8 +1,20 @@
+import styled from 'styled-components'
+
 export default function PageNotFoundContainer() {
   return (
-    <>
+    <Container>
       <h1>404error</h1>
       <p>page not found</p>
-    </>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  background: white;
+  text-align: center;
+  height: 659px;
+  h1 {
+    display: inline-block;
+    margin-top: 270px;
+  }
+`
